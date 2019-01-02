ad3101221168b5c11e9e1be1e5fe63438a751525
Troy Hallock <thallock@gmail.com>
2019-01-01 19:48:42 -0800

    If the short link already exists just return it to the caller

--- app/controllers/short_link_controller.rb
+++ app/controllers/short_link_controller.rb
@@ -2,10 +2,16 @@ class ShortLinkController < ApplicationController
 
   def create
     short_link_params = params.permit(:long_url)
-    short_link = ShortLink.create(short_link_params)
-    short_link.generate_link("#{request.protocol}#{request.host_with_port}")
+    short_link = ShortLink.find_by(long_url: short_link_params[:long_url])
 
-    short_link = short_link.slice(:short_url, :long_url)
-    render json: short_link, status: :created
+    if short_link.present?
+      render json: short_link, status: :ok if short_link.present?
+    else
+      short_link = ShortLink.create(short_link_params)
+      short_link.generate_link("#{request.protocol}#{request.host_with_port}")
+
+      short_link = short_link.slice(:short_url, :long_url)
+      render json: short_link, status: :created
+    end
   end
 end
