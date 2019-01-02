e04c3463499c82738ef3cc8f6f7922f6f45f6a64
Troy Hallock <thallock@gmail.com>
2019-01-01 19:27:29 -0800

    Add the short url to the POST response

--- app/controllers/short_link_controller.rb
+++ app/controllers/short_link_controller.rb
@@ -1,9 +1,11 @@
 class ShortLinkController < ApplicationController
 
   def create
-    short_link_params = params.permit(:short_link)
+    short_link_params = params.permit(:long_url)
     short_link = ShortLink.create(short_link_params)
+    short_link.generate_link("#{request.protocol}#{request.host_with_port}")
 
+    short_link = short_link.slice(:short_url, :long_url)
     render json: short_link, status: :created
   end
 end
--- spec/requests/short_link_spec.rb
+++ spec/requests/short_link_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe 'Short Link API', type: :request do
   describe 'POST /short_link' do
     context 'No short link exists for the given long url' do
       let(:long_url) { "http://www.kapost.com" }
+      let(:short_url_regex) { /http:\/\/www.example.com\/\d+/ }  # RSpec uses www.example.com as the hostname for request specs
 
       it 'Creates a new short link' do
         post '/short_link', params: { long_url: long_url }
 
         expect(response.content_type).to eq("application/json")
         expect(response).to have_http_status(:created)
+
+        json = JSON.parse(response.body)
+        expect(json['long_url']).to eq(long_url)
+        expect(json['short_url']).to match(short_url_regex)
       end
     end
   end
