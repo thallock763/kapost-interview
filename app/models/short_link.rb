b605f93ca6bb002cf2a890a4088feb6f9a66833d
Troy Hallock <thallock@gmail.com>
2019-01-01 19:01:11 -0800

    Add a method to generate a short url. For now it will just use the database id.

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,2 +1,6 @@
 class ShortLink < ApplicationRecord
+
+  def generate_link(url_prefix)
+    self.update(short_url: "#{url_prefix}/#{self.id}")
+  end
 end
--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "generate_link" do
+    let(:long_url) { "http://www.kapost.com" }
+    let!(:short_link) { ShortLink.create(long_url: long_url) }
+    let(:short_url_regex) { /http:\/\/localhost:3000\/\d+/ }
+    let(:url_prefix) { "http://localhost:3000" }
+
+    subject do
+      short_link.generate_link(url_prefix)
+      short_link
+    end
+
+    it "Generates a short link for the given long url" do
+      expect(subject.short_url).to match(short_url_regex)
+    end
+  end
 end
